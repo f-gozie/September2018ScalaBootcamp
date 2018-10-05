99711cdb22746ff58ec9001ae70582f5adae0bc3
Agozie <fagozie43@gmail.com>
2018-10-05 17:18:57 +0100

    Completed the Difference of Squares Exercise

--- src/main/scala/DifferenceOfSquares.scala
+++ src/main/scala/DifferenceOfSquares.scala
@@ -1,8 +1,48 @@
+import scala.math.pow
+
 object DifferenceOfSquares{
 
-  def sumOfSquares(n: Int): Int = ???
+  def sumOfSquares(n: Int): Int = 
+  {
+    var lim = n
+    var totalsum = 0
+    while (lim > 0)
+    {
+      var powered: Int = (pow(lim, 2)).toInt
+      totalsum = totalsum + powered
+      lim = lim-1
+    }
+    totalsum
+  }
 
-  def squareOfSum(n: Int): Int = ???
+  def squareOfSum(n: Int): Int = 
+  {
+    var lim = n
+    var totalsum = 0
+    while (lim > 0)
+    {
+      totalsum = totalsum + lim
+      lim = lim-1
+    }
+    var squareSum: Int = pow(totalsum, 2).toInt
+    squareSum
+  }
 
-  def differenceOfSquares(n: Int):Int = ???
+  def differenceOfSquares(n: Int):Int = 
+  {
+    var sqOfsum = squareOfSum(n)
+    var sumOfsq = sumOfSquares(n)
+
+    var totalDifference = sqOfsum - sumOfsq
+    totalDifference
+  }
 }
+
+// Testing the code
+
+var sumOfsq = DifferenceOfSquares.sumOfSquares(10)
+var sqOfsum = DifferenceOfSquares.squareOfSum(10)
+
+println(DifferenceOfSquares.squareOfSum(10))
+println(DifferenceOfSquares.sumOfSquares(10))
+println(DifferenceOfSquares.differenceOfSquares(10))
